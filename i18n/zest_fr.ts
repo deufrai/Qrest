36e5cf7dd6373128fa65684300c983444e218199
Frederic CORNU <fcornu@wardsback.org>
2008-11-04 12:48:38 +0000

    Got rid of unused action "about Qt..." : dropped associated translation as well
    svn path=/app/trunk/; revision=41

--- i18n/zest_fr.ts
+++ i18n/zest_fr.ts
@@ -4,18 +4,22 @@
 <context>
     <name>ZestAboutDialog</name>
     <message>
+        <location filename="../src/gui/forms/zestaboutdialog.ui" line="16"/>
         <source>About Zest</source>
         <translation>A propos de Zest</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestaboutdialog.ui" line="53"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
+        <location filename="../src/gui/widgets/zestaboutdialog.cpp" line="40"/>
         <source>Author</source>
         <translation>Auteur</translation>
     </message>
     <message>
+        <location filename="../src/gui/widgets/zestaboutdialog.cpp" line="43"/>
         <source>Website</source>
         <translation>Site Web</translation>
     </message>
@@ -23,14 +27,17 @@
 <context>
     <name>ZestMainWindow</name>
     <message>
+        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="56"/>
         <source>I&apos;m ready, sir</source>
         <translation>A vos ordres, chef</translation>
     </message>
     <message>
+        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="152"/>
         <source>You&apos;re steady</source>
         <translation>Vous êtes stable</translation>
     </message>
     <message>
+        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="157"/>
         <source>Keep tapping...</source>
         <translation>Continuez à cliquer...</translation>
     </message>
@@ -38,90 +45,102 @@
 <context>
     <name>ZestMainWindowClass</name>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="25"/>
         <source>ZEST</source>
         <translation>ZEST</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="40"/>
         <source>Tempo Input</source>
         <translation>Tempo</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="46"/>
         <source>Temp&amp;o  : </source>
         <translation>Temp&amp;o  : </translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="68"/>
         <source>Enter tempo in BPM and hit &apos;enter&apos; or use mouse wheel</source>
         <translation>Saisissez le tempo en BPM et validez avec &apos;Entrée&apos; ou utilisez la molette de la souris</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="75"/>
         <source>Click me along tempo until you&apos;re told that you&apos;re tapping steadily</source>
         <translation>Cliquez en suivant les temps jusqu&apos;à ce que l&apos;indicateur de stabilité passe au vert</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="78"/>
         <source>T&amp;AP</source>
         <translation>T&amp;AP</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="101"/>
         <source>Alteration</source>
         <translation>Alteration</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="107"/>
         <source>&amp;Plain</source>
         <translation>&amp;Normale</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="117"/>
         <source>&amp;Dotted</source>
         <translation>&amp;Pointée</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="124"/>
         <source>&amp;Triplet</source>
         <translation>&amp;Triolet</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="134"/>
         <source>Delays and LFOs</source>
         <translation>Delais et LFOs</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="142"/>
         <source>ms</source>
         <translation>ms</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="152"/>
         <source>Hz</source>
         <translation>Hz</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="507"/>
         <source>&amp;Help</source>
         <translation>&amp;Aide</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="513"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <source>About &amp;Qt...</source>
-        <translation>A propos de &amp;Qt...</translation>
-    </message>
-    <message>
-        <source>About Qt</source>
-        <translation>A propos de Qt</translation>
-    </message>
-    <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="532"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="535"/>
         <source>Quit Zest</source>
         <translation>Quitter Zest</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="538"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="543"/>
         <source>About &amp;Zest...</source>
         <translation>A propos de &amp;Zest...</translation>
     </message>
     <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="546"/>
         <source>About Zest</source>
         <translation>A propos de Zest</translation>
     </message>
