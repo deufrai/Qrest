10f367e882d334d1baf74a50058d131812072a6d
Frederic CORNU <fcornu@wardsback.org>
2008-11-09 12:30:01 +0000

    Help window visibility and lifecycle is handled by main app window
    svn path=/app/trunk/; revision=80

--- i18n/zest_fr.ts
+++ i18n/zest_fr.ts
@@ -40,12 +40,12 @@
         <translation>A vos ordres, chef</translation>
     </message>
     <message>
-        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="150"/>
+        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="151"/>
         <source>You&apos;re steady</source>
         <translation>Vous êtes stable</translation>
     </message>
     <message>
-        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="155"/>
+        <location filename="../src/gui/widgets/zestmainwindow.cpp" line="156"/>
         <source>Keep tapping...</source>
         <translation>Continuez à cliquer...</translation>
     </message>
@@ -53,114 +53,114 @@
 <context>
     <name>ZestMainWindowClass</name>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="25"/>
-        <source>ZEST</source>
-        <translation>ZEST</translation>
-    </message>
-    <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="40"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="44"/>
         <source>Tempo Input</source>
         <translation>Tempo</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="46"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="50"/>
         <source>Temp&amp;o  : </source>
         <translation>Temp&amp;o  : </translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="68"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="72"/>
         <source>Enter tempo in BPM and hit &apos;enter&apos; or use mouse wheel</source>
         <translation>Saisissez le tempo en BPM et validez avec &apos;Entrée&apos; ou utilisez la molette de la souris</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="78"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="82"/>
         <source>T&amp;AP</source>
         <translation>T&amp;AP</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="107"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="111"/>
         <source>&amp;Plain</source>
         <translation>&amp;Normale</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="117"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="121"/>
         <source>&amp;Dotted</source>
         <translation>&amp;Pointée</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="124"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="128"/>
         <source>&amp;Triplet</source>
         <translation>&amp;Triolet</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="134"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="138"/>
         <source>Delays and LFOs</source>
         <translation>Delais et LFOs</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="142"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="146"/>
         <source>ms</source>
         <translation>ms</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="152"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="156"/>
         <source>Hz</source>
         <translation>Hz</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="507"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="511"/>
         <source>&amp;Help</source>
-        <translation>&amp;Aide</translation>
+        <translation>Ai&amp;de</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="514"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="518"/>
         <source>&amp;File</source>
         <translation>&amp;Fichier</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="533"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="541"/>
         <source>&amp;Quit</source>
         <translation>&amp;Quitter</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="536"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="544"/>
         <source>Quit Zest</source>
         <translation>Quitter Zest</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="539"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="547"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+Q</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="544"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="556"/>
         <source>About &amp;Zest...</source>
         <translation>A propos de &amp;Zest...</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="547"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="559"/>
         <source>About Zest</source>
         <translation>A propos de Zest</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="75"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="79"/>
         <source>Click in sync with beats until steadiness indicator goes green</source>
         <translation>Cliquez en synchro avec les temps jusqu&apos;à ce que l&apos;indicateur de stabilité passe au vert</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="101"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="105"/>
         <source>Variants</source>
         <translation>Variantes</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="552"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="568"/>
         <source>&amp;Help...</source>
         <translation>&amp;Aide...</translation>
     </message>
     <message>
-        <location filename="../src/gui/forms/zestmainwindow.ui" line="555"/>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="571"/>
         <source>Help</source>
         <translation>Aide</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/forms/zestmainwindow.ui" line="25"/>
+        <source>Zest</source>
+        <translation>Zest</translation>
     </message>
 </context>
 </TS>
