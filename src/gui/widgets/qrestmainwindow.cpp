d2fbf707cabc0498002f2b5041cca701c6fa8dd2
Frederic CORNU <fcornu@wardsback.org>
2009-04-16 07:52:03 +0000

    updated all copyright notices for 2009
    svn path=/app/branches/v0.4/; revision=166

--- src/gui/widgets/qrestmainwindow.cpp
+++ src/gui/widgets/qrestmainwindow.cpp
@@ -1,6 +1,6 @@
 /*  qrest
  *
- *  Copyright (C) 2008 - Frédéric CORNU
+ *  Copyright (C) 2008-2009 - Frédéric CORNU
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
