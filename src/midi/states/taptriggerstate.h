4c8c291bb18c1465b633e0049cb008efb047baec
Frederic CORNU <fcornu@wardsback.org>
2012-04-22 19:41:00 +0200

    [QREST-26]:Forgot a few crucial files ... IN PROGRESS-TAP button click triggered by MIDI events http://requests.wardsback.org/browse/QREST-26

---
+++ src/midi/states/taptriggerstate.h
@@ -0,0 +1,40 @@
+/*  qrest
+ *
+ *  Copyright (C) 2008-2012 - Frédéric CORNU
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TAPTRIGGERSTATE_H_
+#define TAPTRIGGERSTATE_H_
+
+#include "midistate.h"
+#include "../events/midievent.h"
+
+/**
+ * the state in which we are listening for incomming MIDI events
+ * and trigger a TAP if current event matches a user-defined reference event
+ */
+class TapTriggerState: public MidiState {
+
+public:
+    TapTriggerState();
+    virtual ~TapTriggerState();
+
+    virtual void reset() {} // Don't think we'll do much in here
+
+    virtual void processEvent(const MidiEvent* event);
+};
+
+#endif /* TAPTRIGGERSTATE_H_ */
