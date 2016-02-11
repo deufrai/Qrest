3fe6b6caa53a996928d8859bc0a88b8a93426e43
Frédéric CORNU <fcornu@wardsback.org>
2016-02-11 23:03:10 +0100

    QREST-1: update copyright years

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  *  qest
  *
- *  Copyright (C) 2008-2013 - Frédéric CORNU
+ *  Copyright (C) 2008-2016 - Frédéric CORNU
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
